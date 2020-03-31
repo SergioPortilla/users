f30c838ceb1db95b2806d63f51e59c81dbdfd26f
Wilmer Llamas Rensa - Ceiba Software <wilmer.llamas@ceiba.com.co>
2020-03-31 12:19:19 -0500

    Use environment variables to connect to postgres database container

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/ceiba-meli-demo/users
 
 go 1.14
+
+require (
+	github.com/gin-gonic/gin v1.6.2
+	github.com/jinzhu/gorm v1.9.12
+)
--- infrastructure/database_client/client.go
+++ infrastructure/database_client/client.go
@@ -2,27 +2,37 @@ package database_client
 
 import (
 	"fmt"
+	"os"
+	"strconv"
+
 	"github.com/ceiba-meli-demo/users/infrastructure/adapters/repository/models"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 const (
-	UserName = "root"
-	Password = "root"
-	DBName   = "users"
-	Port     = 5432
-	HostName = "localhost"
+	envUserName = "POSTGRES_USER"
+	envPassword = "POSTGRES_PASSWORD"
+	envDBName   = "POSTGRES_DB"
+	envPort     = "POSTGRES_PORT"
+	envHostName = "POSTGRES_HOST"
+)
+
+var (
+	UserName = os.Getenv(envUserName)                       // "root"
+	Password = os.Getenv(envPassword)                       // "root"
+	DBName   = os.Getenv(envDBName)                         // "users"
+	Port, _  = strconv.ParseInt(os.Getenv(envPort), 10, 64) // 5432
+	HostName = os.Getenv(envHostName)                       // "postgres"
 )
 
 func GetDatabaseInstance() *gorm.DB {
-
 	dataSource := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=disable", HostName, Port, UserName, DBName, Password)
-	db, err := gorm.Open("postgres", dataSource)
 
+	db, err := gorm.Open("postgres", dataSource)
 	if err != nil {
 		_ = db.Close()
-		panic("database not working")
+		panic(err)
 	}
 
 	db.SingularTable(true)
