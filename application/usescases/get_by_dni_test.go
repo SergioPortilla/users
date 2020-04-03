774b79a7857bda3626f8182c758f73843149ce56
sebasFranco <jsfrancor2@gmail.com>
2020-04-03 12:03:30 -0500

    Crear Pruebas de Users #15

---
+++ application/usescases/get_by_dni_test.go
@@ -0,0 +1,29 @@
+package usescases
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/mock"
+)
+
+type useCaseGetByDniMock struct {
+	mock.Mock
+}
+
+func (m *useCaseGetByDniMock) GetByDni(DNI int64) bool {
+	args := m.Called(DNI)
+	return args.Bool(0)
+}
+
+func TestGetByDni(t *testing.T) {
+	getUserByDni := new(useCaseGetByDniMock)
+
+	getUserByDni.On("Get user by dni", 1031142378).Return(true)
+
+	getByDniUseCase := GetByDniUseCase{getUserByDni}
+
+	getByDniUseCase.Handler(1031142378)
+
+	getUserByDni.AssertExpectations(t)
+
+}
